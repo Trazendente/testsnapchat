6abdffa67c0f226f5beb97672a0e492c20c5b5d3
Trazendente <137651797+Trazendente@users.noreply.github.com>
2024-09-16 14:56:50 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,21 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development', // or 'production' depending on your needs
   entry: './src/main.js',
   output: {
     filename: 'packed.js',
     path: path.resolve(__dirname, 'docs'),
   },
   optimization: {
-    minimize: false
+    minimize: false,
   },
   devServer: {
     static: {
       directory: path.join(__dirname, 'docs'),
     },
     client: {
-      overlay: false
+      overlay: false,
     },
     compress: true,
     port: 9000,
