318d03d0ce99a619dc2e02e399d58702bddfaef4
Trazendente <137651797+Trazendente@users.noreply.github.com>
2024-10-28 10:42:33 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,24 +1,46 @@
-import { bootstrapCameraKit } from '@snap/camera-kit';
-
-(async function () {
-  const cameraKit = await bootstrapCameraKit({
-    apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzI1OTk1ODg4LCJzdWIiOiJmZTBlYTZkNS0wNjkyLTQ3MGEtYmVlNi1kNDQ1NDkyY2ZhNzd-U1RBR0lOR34zNTMzYzE1Ny1lZTM1LTRjNGYtYmVjZC0xODgwZGY1NTlmODcifQ.ljYneRMTS0ujT7H6VcPC-3Mw3gAa8zqx4XDzoOLhNv8',
-  });
-  const liveRenderTarget = document.getElementById(
-    'canvas'
-  ) as HTMLCanvasElement;
-  const session = await cameraKit.createSession({ liveRenderTarget });
-  const mediaStream = await navigator.mediaDevices.getUserMedia({
-    video: true,
-  });
-
-  await session.setSource(mediaStream);
-  await session.play();
-
-  const lens = await cameraKit.lensRepository.loadLens(
-    '7f650cbe-90f4-4944-a6f5-c2fe1d98a5d6',
-    '98e7af62-b1ab-444b-820d-44250f1c3d45'
-  );
-
-  await session.applyLens(lens);
-})();
+// Import the necessary Camera Kit modules.
+import {
+    bootstrapCameraKit,
+    createMediaStreamSource,
+    Transform2D,
+  } from '@snap/camera-kit';
+  
+  // Create an async function to initialize Camera Kit and start the video stream.
+  (async function() {
+    // Bootstrap Camera Kit using your API token.
+    const cameraKit = await bootstrapCameraKit({
+      apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzI1OTk1ODg4LCJzdWIiOiJmZTBlYTZkNS0wNjkyLTQ3MGEtYmVlNi1kNDQ1NDkyY2ZhNzd-U1RBR0lOR34zNTMzYzE1Ny1lZTM1LTRjNGYtYmVjZC0xODgwZGY1NTlmODcifQ.ljYneRMTS0ujT7H6VcPC-3Mw3gAa8zqx4XDzoOLhNv8'
+    });
+  
+    // Create a new CameraKit session.
+    const session = await cameraKit.createSession();
+  
+    // Replace the `canvas` element with the live output from the CameraKit session.
+    document.getElementById('canvas').replaceWith(session.output.live);
+  
+    // Load the specified lens group.
+    const { lenses } = await cameraKit.lensRepository.loadLensGroups(['6c393bba-55f7-4068-bf4c-6033c0c0097a'])
+  
+    // Apply the first lens in the lens group to the CameraKit session.
+    session.applyLens(lenses[0]);
+  
+    // Get the user's media stream.
+    let mediaStream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: 'environment' }
+    });
+  
+    // Create a CameraKit media stream source from the user's media stream.
+    const source = createMediaStreamSource(
+      mediaStream, { cameraType: 'back' }
+    );
+  
+    // Set the source of the CameraKit session.
+    await session.setSource(source);
+  
+    // Set the render size of the CameraKit session to the size of the browser window.
+    session.source.setRenderSize( window.innerWidth,  window.innerHeight);
+  
+    // Start the CameraKit session.
+    session.play();
+  })();
+  
